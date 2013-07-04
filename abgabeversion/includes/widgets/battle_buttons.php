3a245841abf477419572b8d383eab44771ca7272
Jens Helle <jenshelle@web.de>
2013-07-05 00:30:39 +0200

    buh

---
+++ abgabeversion/includes/widgets/battle_buttons.php
@@ -0,0 +1,6 @@
+<div style="margin-top:auto; margin-bottom:auto; text-align:center;">
+	<input id="battleButton" type="button" value="Attack 1" onclick="">
+	<input id="battleButton" type="button" value="Attack 2" onclick="">
+	<input id="battleButton" type="button" value="Attack 3" onclick="">
+	<input id="battleButton" type="button" value="Attack 4" onclick="">
+</div>
